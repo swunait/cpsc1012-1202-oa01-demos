8934abc56dd177c7c45d2cf7842deb6a89adcfbe
Sam Wu <swu@nait.ca>
2021-02-01 14:38:18 -0700

    Added warm up exercises for if-else control structure

--- CPSC1012-1202-OA01-DemoProjects/PollutionIndex/Program.cs
+++ CPSC1012-1202-OA01-DemoProjects/PollutionIndex/Program.cs
@@ -6,7 +6,29 @@ namespace PollutionIndex
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const double CUTOFF = 50.0;      // Cutoff for hazardous condition
+
+            double level1, level2, level3,      // Pollution level readings
+                   index;                       // Average of readings
+
+            Console.WriteLine("Enter 3 pollution level readings: ");
+            level1 = double.Parse(Console.ReadLine());
+            level2 = double.Parse(Console.ReadLine());
+            level3 = double.Parse(Console.ReadLine());
+
+            index = (level1 + level2 + level3) / 3;
+
+            // Check for hazardous condition.
+            //if ___________________
+            //{
+            //    Console.WriteLine("Hazardous condition");
+
+            //}    
+            //___________________
+            //{
+            //    Console.WriteLine("Non-hazardous condition");
+            //}
+
         }
     }
 }
