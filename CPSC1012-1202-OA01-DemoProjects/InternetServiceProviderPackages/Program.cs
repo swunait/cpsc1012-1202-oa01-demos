cec4334389bed54076d802f969fd0ed5fd02c7c0
Sam Wu <swu@nait.ca>
2021-02-01 21:18:45 -0700

    Updated solution for ISP

--- CPSC1012-1202-OA01-DemoProjects/InternetServiceProviderPackages/Program.cs
+++ CPSC1012-1202-OA01-DemoProjects/InternetServiceProviderPackages/Program.cs
@@ -12,7 +12,7 @@
  *          B           $13.95/month for first 20 hours plus $1.00/hour after the first 20 hours
  *          C           $19.95/month
  *          
- * Output:  Total cost for a chose Internet package.
+ * Output:  Total cost for a chosen Internet package.
  * 
  * */
 
@@ -24,14 +24,14 @@ namespace InternetServiceProviderPackages
     {
         static void Main(string[] args)
         {
-            // Declare variable for package letter (char)
+            // Declare variable for package letter (char) and billing cost (string)
             char packageLetter;
-
+            string billingCost;
             // Prompt and read in te package letter
             Console.WriteLine("Please select one of the following packages:");
             Console.WriteLine("A - $9.95/month for first 10 hours plus $2.00/hour after the first 10 hours");
             Console.WriteLine("B - $13.95/month for first 20 hours pluse $1.00/hour after the first 20 hours");
-            Console.WriteLine("C -  $19.95/month");
+            Console.WriteLine("C - $19.95/month");
             packageLetter = char.Parse(Console.ReadLine());
 
             // Lookup the table for the total cost for the selected package
@@ -53,7 +53,8 @@ namespace InternetServiceProviderPackages
                             totalCost = totalCost + hourlyCost;
                         }
                         // Display finalcost
-                        Console.WriteLine($"Package A total cost is {totalCost}");
+                        //Console.WriteLine($"Package A total cost is {totalCost}");
+                        billingCost = $"Package A total cost is {totalCost}";
                     }
                     break;
                 case 'B':   // Package B
@@ -70,20 +71,24 @@ namespace InternetServiceProviderPackages
                             totalCost = totalCost + hourlyCost;
                         }
                         // Display finalcost
-                        Console.WriteLine($"Package B total cost is {totalCost}");
+                        //Console.WriteLine($"Package B total cost is {totalCost}");
+                        billingCost = $"Package B total cost is {totalCost}";
                     }
                     break;
                 case 'C':   // Package C
                     {
-                        Console.WriteLine("For package C, you monthly cost is $19.95/month.");
+                        //Console.WriteLine("For package C, you monthly cost is $19.95/month.");
+                        billingCost = "For package C, you monthly cost is $19.95/month.";
                     }
                     break;
                 default:    // Invalid package selection
-                    Console.WriteLine("Invalid menu choice - you must enter A, B, or C");
+                    //Console.WriteLine("Invalid menu choice - you must enter A, B, or C");
+                    billingCost = "Invalid menu choice - you must enter A, B, or C";
                     break;
             }
 
             // Display the total cost if a valid package letter is entered
+            Console.WriteLine(billingCost);
 
         }
     }
