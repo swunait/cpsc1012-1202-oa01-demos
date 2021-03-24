466fe6ed138370aa557479927b35f1344c8a3ee5
Sam Wu <swu@nait.ca>
2021-03-24 16:45:26 -0600

    added write to file

--- CPSC1012-1202-OA01-DemoProjects/HockerPlayerInfo/Program.cs
+++ CPSC1012-1202-OA01-DemoProjects/HockerPlayerInfo/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 using System.Collections.Generic;   // for List class
+using System.IO;    // for StreamReader and StreamWriter
 
 namespace HockerPlayerInfo
 {
@@ -51,18 +52,35 @@ namespace HockerPlayerInfo
             }
         }
 
+        static void WriteToFile(List<HockeyPlayer> playerList, string outputFilePath)
+        {
+            StreamWriter writer = new StreamWriter(outputFilePath); // Create a new file to write to
+            foreach(HockeyPlayer singlePlayer in playerList)
+            {
+                writer.Write(singlePlayer.Name);
+                writer.Write(",");
+                writer.Write(singlePlayer.Number);
+                writer.Write(",");
+                writer.WriteLine(singlePlayer.Position);
+            }
+            writer.Close();
+
+        }
+
         static void DisplayMainMenu()
         {
             Console.WriteLine("1. Add New Hockey Player");
             Console.WriteLine("2. Display all Hockey Player");
-            Console.WriteLine("3. Save Hockey Player list to a file");
-            Console.WriteLine("4. Load Player List with data from a file");
+            Console.WriteLine("3. Save list to a file");
+            Console.WriteLine("4. Load list from a file");
             Console.WriteLine("0. Quit program");
         }
         static void Main(string[] args)
         {
             // Declare and create a new List of HockeyPlayer
             List<HockeyPlayer> playerList = new List<HockeyPlayer>();
+            string outputFilePath = @"C:\temp\hockey-players.csv";
+
             int menuChoice; // To hold menu choice
             const int QuitProgramChoice = 0;
             int playerCount = 0;
