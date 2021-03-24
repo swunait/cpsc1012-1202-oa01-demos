6ddf50e005e92efc8a5fa01088271053fd091334
Sam Wu <swu@nait.ca>
2021-03-24 16:28:48 -0600

    Added List of Object example

---
+++ CPSC1012-1202-OA01-DemoProjects/HockerPlayerInfo/HockeyPlayer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HockerPlayerInfo
+{
+    // This class contains the description of a NHL Hockey Player
+    public class HockeyPlayer   // public access modifier allows this class HockeyPlayer to be used outside of the current namespace HockerPlayerInfo
+    {
+        // Define data fields that contains data about a hockey player
+        // private access modifier means the data field is only accessible within this 
+        // current class
+        private string _name;
+        private string _position;
+        private int _number;
+
+        // Define properties to encapsulate access to our data fields
+        // Properties allows you to get (accessor) or set (mutator) a data field
+        public string Name  // Name is a property of the HockeyPlayer
+        {
+            get 
+            {
+                return _name;
+            }
+            set 
+            {
+                _name = value;  // value is keyword for the new value being assigned
+                                // value is only accessible inside the set block
+            }
+        }
+        // Define a property for Position
+        public string Position 
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+        // Define a property for Number
+        public int Number
+        {
+            get { return _number; }
+            set { _number = value; }
+        }
+
+        // Define a constructor to initialize the properties of the current object
+        public HockeyPlayer()   // A constructor has the same name as the class name and no return value
+        {
+            // Assign default/initial values to your data fields
+            _name = "No name assigned";
+            _position = "C";
+            _number = 1;
+        }
+    }
+}
---
+++ CPSC1012-1202-OA01-DemoProjects/HockerPlayerInfo/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+using System.Collections.Generic;   // for List class
+
+namespace HockerPlayerInfo
+{
+    class Program
+    {
+        /// <summary>
+        /// Add a single HockeyPlayer object to the playerList
+        /// </summary>
+        /// <param name="playerList">A list of hockey players</param>
+        /// <returns>The number of players in the list</returns>
+        static int AddHockeyPlayer(List<HockeyPlayer> playerList)
+        {
+            int playerCount = 0;
+            // Create a new HockeyPlayer object
+            HockeyPlayer player1 = new HockeyPlayer();
+            // Prompt and read the Name of the HockeyPlayer
+            Console.Write("Enter the player name: ");
+            player1.Name = Console.ReadLine();
+            // Prompt and read the Number of the HockeyPlayer
+            Console.Write("Enter the player number: ");
+            player1.Number = int.Parse(Console.ReadLine());
+            // Prompt and read the Position of the HockeyPlayer
+            Console.Write("Enter the player position (C,LW,RW,D,G): ");
+            player1.Position = Console.ReadLine();
+            // Add the HockeyPlayer object to playerList
+            playerList.Add(player1);
+            playerCount = playerList.Count;
+
+            return playerCount;
+        }
+
+        /// <summary>
+        /// Display information about each HockeyPlayer in playerList
+        /// </summary>
+        /// <param name="playerList">The list of hocker players</param>
+        static void DisplayHockerPlayers(List<HockeyPlayer> playerList)
+        {
+            foreach(
+                HockeyPlayer    // Collection element data type
+                singlePlayer    // Variable name you assigned represent a element in the collection
+                in              // reserve keyword
+                playerList      // Collection of objects/values to iterate through
+                )
+            {
+                Console.WriteLine($"Player Name: {singlePlayer.Name}");
+                Console.WriteLine($"Player Number: {singlePlayer.Number}");
+                Console.WriteLine($"Player Position: {singlePlayer.Position}");
+            }
+        }
+
+        static void DisplayMainMenu()
+        {
+            Console.WriteLine("1. Add New Hockey Player");
+            Console.WriteLine("2. Display all Hockey Player");
+            Console.WriteLine("3. Save Hockey Player list to a file");
+            Console.WriteLine("4. Load Player List with data from a file");
+            Console.WriteLine("0. Quit program");
+        }
+        static void Main(string[] args)
+        {
+            // Declare and create a new List of HockeyPlayer
+            List<HockeyPlayer> playerList = new List<HockeyPlayer>();
+            int menuChoice; // To hold menu choice
+            const int QuitProgramChoice = 0;
+            int playerCount = 0;
+
+            do
+            {
+                DisplayMainMenu();
+                menuChoice = int.Parse(Console.ReadLine());
+                switch (menuChoice) 
+                {
+                    case 1:
+                        playerCount = AddHockeyPlayer(playerList);
+                        Console.WriteLine($"There are currently {playerCount} players.");
+                        break;
+                    case 2:
+                        DisplayHockerPlayers(playerList);
+                        break;
+                    case 0:
+                        Console.WriteLine("Good-bye");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input value. Try again");
+                        break;
+                }
+            } while (menuChoice != QuitProgramChoice);
+
+        }
+    }
+}
